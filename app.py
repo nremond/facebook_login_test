a6afaec0fa9168065edb94c813d7a186f1f7e4c3
nire <nicolas.remond@gmail.com>
2012-02-16 01:06:29 +0100

    add a @authenticated annotation and try to debug under Eclipse

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, url_for, session, request, flash, render_template
+from flask import Flask, redirect, url_for, session, request, render_template
 from flaskext.oauth import OAuth
 
 
@@ -9,7 +9,7 @@ FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
 FACEBOOK_APP_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
 
 
-app = app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
 oauth = OAuth()
@@ -25,11 +25,19 @@ facebook = oauth.remote_app('facebook',
 )
 
 
+def authenticated(func):
+	def with_logging(*args, **kwargs):
+		print func.__name__ + " was called"
+		if not get_facebook_oauth_token():
+			return redirect(url_for('login'))
+		return func(*args, **kwargs)
+	return with_logging
+
+
 @app.route('/')
+@authenticated
 def index():
-	
 	return render_template('index.html', title="nicolas")
-	#return redirect(url_for('login'))
 
 
 @app.route('/login')
@@ -59,7 +67,10 @@ def facebook_authorized(resp):
 
 @facebook.tokengetter
 def get_facebook_oauth_token():
-	return session.get('oauth_token')
+	if 'oauth_token' in session:
+		return session['oauth_token']
+	else : 
+		return None
 
 @app.route('/hello')
 def hello():
@@ -67,7 +78,12 @@ def hello():
 
 
 
+
+
+
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	use_debugger = False
+	app.run(use_debugger=use_debugger, debug=app.debug,
+            use_reloader=use_debugger,host='0.0.0.0', port=port)
